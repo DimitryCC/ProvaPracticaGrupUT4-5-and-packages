7b88f5cacda24708d6f6cca213683aa60de7ebc0
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-03-08 17:52:06 +0100

    feat :sparkles: Añadir atributos y constructor de clase Artista

--- ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Artistas.java
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Artistas.java
@@ -1,5 +1,17 @@
 package com.Isaac.Dimitry.Clases;
 
+import java.util.ArrayList;
+
 public class Artistas {
-//hola
+	//Atributs
+	private String nomArtista;
+	private String nacionalitat;
+	private ArrayList<Obras> obras = new ArrayList<Obras>();
+	//Constructor
+	
+	public Artistas(String nomArtista, String nacionalitat) {
+		this.nomArtista = nomArtista;
+		this.nacionalitat = nacionalitat;
+	}
+	
 }
