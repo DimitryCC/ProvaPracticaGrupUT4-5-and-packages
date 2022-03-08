8757673b6fdef290ddc2ca32c1b61950625356d5
TomasRuizM <tomasruiz@paucasesnovescifp.cat>
2022-03-08 18:29:56 +0100

    Clases usuario y comentario

--- ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Comentarios.java
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Comentarios.java
@@ -1,5 +1,32 @@
 package com.Isaac.Dimitry.Clases;
 
 public class Comentarios {
-	//hola
+	
+	private Usuari creador;
+	private String comentari;
+	private boolean validacion = false;
+	
+	Comentarios (Usuari creador, String comentari){
+		setCreador(creador);
+		setComentari(comentari);
+	}
+	
+	public Usuari getCreador() {
+		return creador;
+	}
+	public void setCreador(Usuari creador) {
+		this.creador = creador;
+	}
+	public String getComentari() {
+		return comentari;
+	}
+	public void setComentari(String comentari) {
+		this.comentari = comentari;
+	}
+	
+	public boolean validar (Boolean v) {
+		return v = true; 
+	}
+	
+	
 }
--- ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Usuari.java
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Usuari.java
@@ -2,4 +2,18 @@ package com.Isaac.Dimitry.Clases;
 
 public class Usuari {
 
+	
+	private String nombre;
+	
+	
+	Usuari (String nombre){
+		setNombre(nombre);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
