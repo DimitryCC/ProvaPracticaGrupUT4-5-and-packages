bd32044c28518fd9f8f4c9e6605c802619a9d025
didra50 <57315328+didra50@users.noreply.github.com>
2022-03-13 19:49:37 +0100

    solo he añadido el control de errores de artista

--- ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Artistas.java
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Artistas.java
@@ -1,25 +1,57 @@
 package com.Isaac.Dimitry.Clases;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.Isaac.Dimitry.Exeptions.ArtistaExeption;
 
 public class Artistas {
 	//Atributs
 	private String nomArtista;
 	private String nacionalitat;
-	private ArrayList<Obras> obras = new ArrayList<Obras>();
+	private ArrayList<Obras> obras;
 	
-	public Artistas(String nomArtista, String nacionalitat, ArrayList<Obras> obras) {
-		this.nomArtista = nomArtista;
-		this.nacionalitat = nacionalitat;
-		this.obras = obras;
+	public Artistas(String nomArtista, String nacionalitat) throws ArtistaExeption {
+		setNomArtista(nomArtista);
+		setNacionalitat(nacionalitat);
 	}
-	public String getNacionalitat() {
-		return nacionalitat;
+	public void setNomArtista(String nomArtista) throws ArtistaExeption{
+		boolean isValid = false;
+		Pattern pattern = Pattern.compile("(\\b[A-Z]{1}[a-z]+)( )([A-Z]{1}[a-z]+\\b)");
+		Matcher matcher = pattern.matcher(nomArtista);
+		boolean matchFound = matcher.find();
+		if(matchFound) {
+			this.nomArtista = nomArtista;
+		} else {
+			throw new ArtistaExeption("El nombre del artist es incore");
+		}
+	}
+	public void setNacionalitat(String nacionalitat2) throws ArtistaExeption {
+		Pattern pattern = Pattern.compile("^[A-Z][a-z]+( [A-Z][a-z]+)*$");
+	    Matcher matcher = pattern.matcher(nacionalitat2);
+	    boolean matchFound = matcher.find();
+	    if(matchFound) {
+	    	this.nacionalitat=nacionalitat2;
+	    } else {
+	    	throw new ArtistaExeption("El nombre del pais es incorrecto");
+	    }
+	}
+	public void setObras(ArrayList<Obras> obras2) throws ArtistaExeption {
+		ArrayList<Obras> obras = new  ArrayList<Obras>();
+		if (obras2.size()==0) {
+			throw new ArtistaExeption("El llistat de Obres no pot eatar buida");
+		}else {
+			this.obras = obras2;
+		}
 	}
 	public String getNomArtista() {
 		return nomArtista;
 	}
-	public void setNacionalitat(String nacionalitat) {
-		this.nacionalitat = nacionalitat;
+	public String getNacionalitat() {
+		return nacionalitat;
+	}
+	public ArrayList<Obras> getObras() {
+		return obras;
 	}
 }
---
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Exeptions/ArtistaExeption.java
@@ -0,0 +1,29 @@
+package com.Isaac.Dimitry.Exeptions;
+
+public class ArtistaExeption extends Exception {
+
+	public ArtistaExeption() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArtistaExeption(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArtistaExeption(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArtistaExeption(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArtistaExeption(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
