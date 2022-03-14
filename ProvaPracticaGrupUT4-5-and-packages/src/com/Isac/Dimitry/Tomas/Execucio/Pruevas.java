9f803e14307882b0ba2716003cddcc427ede937c
didra50 <57315328+didra50@users.noreply.github.com>
2022-03-14 12:35:33 +0100

    Estado casi finalizado el inicializador

---
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isac/Dimitry/Tomas/Execucio/Pruevas.java
@@ -0,0 +1,18 @@
+package com.Isac.Dimitry.Tomas.Execucio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.Isaac.Dimitry.Clases.Usuari;
+
+public class Pruevas {
+
+	public static void main(String[] args) {
+		ArrayList<Usuari> Users = new ArrayList<Usuari>(); 
+		Controlador Hola = new Controlador();
+		Hola.iniciadorUsuarios(Users);
+		for (Usuari usuari : Users) {
+			System.out.println(usuari.toString());
+		}
+	}
+}
