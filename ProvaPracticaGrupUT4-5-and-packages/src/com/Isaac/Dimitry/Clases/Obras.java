092e79bf6c52b3faa5dcde825f5282f9adbf2448
isaacpaloug <isaacpalou@paucasesnovescifp.cat>
2022-03-14 20:33:14 +0100

    obras

--- ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Obras.java
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Obras.java
@@ -1,4 +1,28 @@
 package com.Isaac.Dimitry.Clases;
 
 public class Obras {
+	private String nomObra;
+	private String modalitat;
+	
+	public Obras(String nomObra, String modalitat) {
+		this.nomObra = nomObra;
+		this.modalitat = modalitat; 
+	}
+
+	public String getNomObra() {
+		return nomObra;
+	}
+
+	public void setNomObra(String nomObra) {
+		this.nomObra = nomObra;
+	}
+
+	public String getModalitat() {
+		return modalitat;
+	}
+
+	public void setModalitat(String modalitat) {
+		this.modalitat = modalitat;
+	}
+	
 }
