645c076dfe7d7e45265642d01adc24e39a61fc40
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-03-15 18:03:42 +0100

    test: Añadir excepciones para la clase OBRAS

--- ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Obras.java
+++ ProvaPracticaGrupUT4-5-and-packages/src/com/Isaac/Dimitry/Clases/Obras.java
@@ -1,5 +1,7 @@
 package com.Isaac.Dimitry.Clases;
 
+import com.Isaac.Dimitry.Exeptions.ObrasExeption;
+
 public class Obras {
 	private String nomObra;
 	private String modalitat;
@@ -13,23 +15,31 @@ public class Obras {
 		return nomObra;
 	}
 
-	public void setNomObra(String nomObra) {
-		this.nomObra = nomObra;
+	public void setNomObra(String nomObra) throws ObrasExeption {
+		if (nomObra.equals(null)) {
+			throw new ObrasExeption("El nom de l'obra es null");
+		}else {
+			this.nomObra = nomObra;
+		}		
 	}
 
 	public String getModalitat() {
 		return modalitat;
 	}
 
-	public void setModalitat(String modalitat) {
-		this.modalitat = modalitat;
+	public void setModalitat(String modalitat) throws ObrasExeption {
+		if (nomObra.equals(null)) {
+			throw new ObrasExeption("El nom de la modalitat es null");
+		}else {
+			this.modalitat = modalitat;
+		}		
 	}
 	
 }
 /*
  *En terminos generales
  *poder enmagatzemar el autor (per aportar redundancia de dades)
- *falta la El control de errores (important es una de les clases especificades coma minim es adir obligatoria)
+ *falta la El control de errores (important es una de les clases especificades coma minim es añadir obligatoria)
  * En termes especificas faltan que pugi amagatzamar imatges
  * Funcio per mostrar el nom y modalitat
  */
